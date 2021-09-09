270b14c00ac5d71af08a9eaaab4e4f23b5f78780
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-09-09 08:11:11 +0700

    fix softdelete

--- src/models/historyModel.js
+++ src/models/historyModel.js
@@ -6,14 +6,14 @@ const getHistory = (search, userId, order, start = '', limit = '') => new Promis
   if (userId === 0) {
     if (start === '' && limit === '') {
       connection.query(
-        `select order_id as order_history_id, status_order, total_price, (select product_name from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as product_name, (select img_product from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as img_product from orders where invoice_number LIKE '%${search}%' order by created_at ${order}`,
+        `select order_id as order_history_id, status_order, total_price, (select product_name from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as product_name, (select img_product from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as img_product from orders where deleted = 0 and invoice_number LIKE '%${search}%' order by created_at ${order}`,
         (err, result) => {
           promiseResolveReject(resolve, reject, err, result);
         },
       );
     } else {
       connection.query(
-        `select order_id as order_history_id, status_order, total_price, (select product_name from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as product_name, (select img_product from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as img_product from orders where invoice_number LIKE '%${search}%' order by created_at ${order} limit ${start}, ${limit}`,
+        `select order_id as order_history_id, status_order, total_price, (select product_name from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as product_name, (select img_product from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as img_product from orders where deleted = 0 and invoice_number LIKE '%${search}%' order by created_at ${order} limit ${start}, ${limit}`,
         (err, result) => {
           promiseResolveReject(resolve, reject, err, result);
         },
@@ -21,14 +21,14 @@ const getHistory = (search, userId, order, start = '', limit = '') => new Promis
     }
   } else if (start === '' && limit === '') {
     connection.query(
-      `select order_id as order_history_id, status_order, total_price, (select product_name from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as product_name, (select img_product from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as img_product from orders where user_id = ${userId} and invoice_number LIKE '%${search}%' order by created_at ${order}`,
+      `select order_id as order_history_id, status_order, total_price, (select product_name from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as product_name, (select img_product from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as img_product from orders where deleted = 0 and user_id = ${userId} and invoice_number LIKE '%${search}%' order by created_at ${order}`,
       (err, result) => {
         promiseResolveReject(resolve, reject, err, result);
       },
     );
   } else {
     connection.query(
-      `select order_id as order_history_id, status_order, total_price, (select product_name from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as product_name, (select img_product from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as img_product from orders where user_id = ${userId} and invoice_number LIKE '%${search}%' order by created_at ${order} limit ${start}, ${limit}`,
+      `select order_id as order_history_id, status_order, total_price, (select product_name from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as product_name, (select img_product from order_detail inner join products on order_detail.product_id = products.product_id where order_id = order_history_id order by order_detail_id desc limit 1) as img_product from orders where deleted = 0 and user_id = ${userId} and invoice_number LIKE '%${search}%' order by created_at ${order} limit ${start}, ${limit}`,
       (err, result) => {
         promiseResolveReject(resolve, reject, err, result);
       },
