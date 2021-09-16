f8a3aed2393900972f6c568e70515b7a97c5dfca
dwinovic <nd.cahya19@gmail.com>
2021-09-16 10:03:30 +0700

    setup redis

--- src/configs/redis.js
+++ src/configs/redis.js
@@ -1,9 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import Redis from 'ioredis';
 
-export const redis = new Redis({
-  port: process.env.PORT_REDIS,
-  host: process.env.HOST_REDIS,
-  password: process.env.AUTH_REDIS,
-  db: 0,
+export const client = new Redis(process.env.REDIS_URL, {
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
+// export const redis = new Redis({
+//   port: process.env.PORT_REDIS,
+//   host: process.env.HOST_REDIS,
+//   password: process.env.AUTH_REDIS,
+//   db: 0,
+// });
